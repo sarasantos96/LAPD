c8a468a959335144c7617d6df6f14ecc2a5892d3
Nuno Miguel <up201406990@fe.up.pt>
2018-04-17 16:45:56 +0100

    Signs in and redirects to maps

--- app/src/main/java/com/getout/activities/SignInActivity.java
+++ app/src/main/java/com/getout/activities/SignInActivity.java
@@ -1,28 +1,21 @@
 package com.getout.activities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.getout.R;
+import com.getout.google.GoogleGlobals;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private GoogleSignInOptions gso;
-    private GoogleSignInClient gsc;
-    private GoogleSignInAccount gsa;
 
     private TextView mStatusTextView;
 
@@ -32,20 +25,19 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
-        mStatusTextView = findViewById(R.id.info);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        GoogleGlobals.options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
-        gsc = GoogleSignIn.getClient(this, gso);
+        GoogleGlobals.client = GoogleSignIn.getClient(this, GoogleGlobals.options);
 
 
         //TEST
-        Button button = (Button) findViewById(R.id.testMapsButton);
+        /*Button button = (Button) findViewById(R.id.testMapsButton);
 
         // Capture button clicks
         button.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +48,7 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                         MapsActivity.class);
                 startActivity(myIntent);
             }
-        });
+        });*/
     }
 
     @Override
@@ -65,12 +57,12 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
-        gsa = GoogleSignIn.getLastSignedInAccount(this);
+        GoogleGlobals.account = GoogleSignIn.getLastSignedInAccount(this);
 
-        if(gsa == null){
-            System.out.println("Faz login");
+        if(GoogleGlobals.account == null){
+            //TODO Show login button
         }else{
-            updateUI(gsa);
+            updateUI(GoogleGlobals.account);
         }
     }
 
@@ -80,33 +72,21 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.sign_in_button:
                 signIn();
                 break;
-            case R.id.sign_out_button:
-                signOut();
-                break;
         }
     }
 
     private void signIn() {
-        Intent signInIntent = gsc.getSignInIntent();
-        startActivityForResult(signInIntent, 9000);
+        Intent signInIntent = GoogleGlobals.client.getSignInIntent();
+        startActivityForResult(signInIntent, 9001);
     }
 
-    private void signOut() {
-        gsc.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        updateUI(gsa);
-                    }
-                });
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 9000) {
+        if (requestCode == 9001) {
             // The Task returned from this call is always completed, no need to attach
             // a listener.
 
@@ -117,10 +97,10 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
-            gsa = completedTask.getResult(ApiException.class);
+            GoogleGlobals.account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            updateUI(gsa);
+            updateUI(GoogleGlobals.account);
         } catch (ApiException e) {
             // The ApiException status code indimStatusTextViewcates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -131,15 +111,11 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     private void updateUI(GoogleSignInAccount account) {
         if (account != null) {
-            mStatusTextView.setText(account.getDisplayName());
-
-            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+            //TODO redirect to maps activity
+            Intent i = new Intent(SignInActivity.this, MapsActivity.class);
+            startActivity(i);
         } else {
-            mStatusTextView.setText("No account");
-
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
         }
     }
 
---
+++ app/src/main/java/com/getout/google/GoogleGlobals.java
@@ -0,0 +1,11 @@
+package com.getout.google;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class GoogleGlobals {
+    public static GoogleSignInOptions options;
+    public static GoogleSignInClient client;
+    public static GoogleSignInAccount account;
+}
