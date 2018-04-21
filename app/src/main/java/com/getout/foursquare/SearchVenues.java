b418874adb00647bc1f49707869ac10553feedfc
Sara Santos <santos.sara96@gmail.com>
2018-04-21 19:25:16 +0100

    Revert ":trollface:"
    This reverts commit fbbba2ecd120eda6f7524530a1c9dc8bd033b2c6.

--- app/src/main/java/com/getout/foursquare/SearchVenues.java
+++ app/src/main/java/com/getout/foursquare/SearchVenues.java
@@ -2,7 +2,6 @@ package com.getout.foursquare;
 
 import android.app.Activity;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.getout.activities.MapsActivity;
@@ -13,10 +12,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 import org.json.JSONArray;
@@ -43,20 +40,14 @@ public class SearchVenues extends AsyncTask<String,Void,String>{
     protected String doInBackground(String... params) {
 
         String result = "";
-        String s_url = null;
-        try {
-            s_url = "https://api.foursquare.com/v2/venues/search?"
-            +"client_id=" + FOURSQUARE_CLIENT_ID
-                    +"&client_secret="+ FOURSQUARE_CLIENT_SECRET
-                    +"&v=20180418"
-                    +"&radius=5000"
-                    +"&intent=browse"
-                    +"&ll=" + params[0]
-                    +"&query=" + URLEncoder.encode(params[1], "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        Log.d("S_URL",s_url);
+        String s_url = "https://api.foursquare.com/v2/venues/search?"
+        +"client_id=" + FOURSQUARE_CLIENT_ID
+                +"&client_secret="+ FOURSQUARE_CLIENT_SECRET
+                +"&v=20180418"
+                +"&radius=5000"
+                +"&intent=browse"
+                +"&ll=" + params[0]
+                +"&query=" + params[1];
         HttpURLConnection conn = null;
         try {
             URL url = new URL(s_url);
