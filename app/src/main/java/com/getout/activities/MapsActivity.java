3f344ad8820da251e4965bfd34864dbd59b4961d
Sara Santos <santos.sara96@gmail.com>
2018-05-01 16:56:27 +0100

    Remove buttons

--- app/src/main/java/com/getout/activities/MapsActivity.java
+++ app/src/main/java/com/getout/activities/MapsActivity.java
@@ -101,13 +101,7 @@ public class MapsActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        return true;
-    }
-
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
