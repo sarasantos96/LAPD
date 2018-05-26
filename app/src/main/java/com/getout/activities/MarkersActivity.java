4773147a1f0472e3b4307831719fb8d779de8f34
Nuno Miguel <up201406990@fe.up.pt>
2018-05-26 14:36:53 +0100

    New task for multiple requests for the venues

--- app/src/main/java/com/getout/activities/MarkersActivity.java
+++ app/src/main/java/com/getout/activities/MarkersActivity.java
@@ -2,23 +2,48 @@ package com.getout.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.getout.R;
+import com.getout.foursquare.FoursquareGlobals;
+import com.getout.foursquare.GetVenueDetails;
+import com.getout.foursquare.GetVenueDetailsArray;
+import com.getout.foursquare.Venue;
 import com.getout.utils.MarkerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MarkersActivity extends AppCompatActivity {
 
-    public MarkerAdapter adapter;
+    public static MarkerAdapter adapter;
+    private List<Venue> venues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_markers);
 
+        this.venues = new ArrayList<>();
+
         ListView markerList = (ListView) findViewById(R.id.markerList);
 
-        adapter = new MarkerAdapter(this);
+        GetVenueDetailsArray f = new GetVenueDetailsArray(MarkersActivity.this);
+        try {
+            f.execute(FoursquareGlobals.VENUE_LIST).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        adapter = new MarkerAdapter(this, venues);
         markerList.setAdapter(adapter);
+    }
+
+    public void addVenue(Venue v) {
+        this.venues.add(v);
     }
 }
--- app/src/main/java/com/getout/utils/MarkerAdapter.java
+++ app/src/main/java/com/getout/utils/MarkerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,24 +14,30 @@ import android.widget.TextView;
 
 import com.getout.R;
 import com.getout.foursquare.FoursquareGlobals;
+import com.getout.foursquare.Venue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkerAdapter extends BaseAdapter{
     private Context context;
+    private List<Venue> venues;
     private LayoutInflater inflater;
 
-    public MarkerAdapter(Context c) {
+    public MarkerAdapter(Context c, List<Venue> venues) {
         this.context = c;
+        this.venues = venues;
         inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return FoursquareGlobals.VENUE_LIST.size();
+        return this.venues.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return FoursquareGlobals.VENUE_LIST.get(position);
+        return this.venues.get(position);
     }
 
     @Override
@@ -42,8 +49,12 @@ public class MarkerAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         View rowView = inflater.inflate(R.layout.list_item_markers, parent, false);
 
-        TextView id = (TextView) rowView.findViewById(R.id.markerId);
-        id.setText((String)getItem(position));
+        TextView id = (TextView) rowView.findViewById(R.id.markerName);
+        Venue v = (Venue)getItem(position);
+
+        Log.e("Venue", v.toString());
+
+        id.setText(v.getName());
 
         return rowView;
     }
